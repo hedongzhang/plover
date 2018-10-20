e3a920d5928572ea16994faabe84e531162e2a0e
hedong.zhang <hdzhang@hdzhang-2.local>
2018-10-20 15:47:04 +0800

    提交demo代码

--- main.py
+++ main.py
@@ -9,23 +9,39 @@ Author: hedong.zhang@woqutech.com
 Description: 
 
 """
+import json
 
-import tornado.web
-
+import tornado
+from tornado import web, httpserver, ioloop
 from tornado.options import define, options
 
-define("port", default=8000, help="run on the given port", type=int)
+define("port", default=443, help="run on the given port", type=int)
 
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        greeting = self.get_argument('greeting', 'Hello')
-        self.write(greeting + ', friendly user!')
+        self.write("hello ")
+
+    def post(self):
+        try:
+            post_vars = self.get_argument("post_vars")
+            request_args = json.loads(post_vars)
+        except:
+            post_vars = json.loads(self.request.body).get("post_vars")
+            request_args = post_vars
+
+        open_id = request_args.get("open_id", 0)
+        name = request_args.get("name", "hdzhang")
+
+        self.write(json.dumps(dict(ret_code=0, status="success", open_id=open_id, name=name)))
 
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
-    http_server = tornado.httpserver.HTTPServer(app)
+    app = tornado.web.Application(handlers=[(r"/user", IndexHandler)])
+
+    ssl_options = dict(certfile="./conf/server.crt", keyfile="./conf/server.key")
+    http_server = tornado.httpserver.HTTPServer(app, ssl_options=ssl_options)
     http_server.listen(options.port)
+
     tornado.ioloop.IOLoop.instance().start()
